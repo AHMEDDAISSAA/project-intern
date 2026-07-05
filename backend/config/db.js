1d822a4f60226928e46402c6ad4bd505c72fffca
AHMEDD AISSA <ahmedaissa575@gmail.com>
2026-07-05 16:28:49 +0100

    Fix: correction connexion MongoDB (bug top-level await)

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/internDB');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connecté ');
   } catch (err) {
     console.error('Erreur de connexion MongoDB :', err);
   }
 }
-
-await mongoose.connect(process.env.MONGO_URI);
 
 module.exports = connectDB;
